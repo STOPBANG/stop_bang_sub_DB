f7481d1c8b4af0018e2dbe35b08e249aea4bbee6
joiejuni <lumiere.298@gmail.com>
2024-04-26 17:55:12 +0900

    feat: 리뷰 아이디, 신고자로 신고 검색하는 기능

--- models/reportModel.js
+++ models/reportModel.js
@@ -1,13 +1,51 @@
 const Report = require('../database/models/tables/report');
+const { findALLById } = require('./bookmarkModel');
 
 module.exports = {
+  findAllById: async (req, res) => {
+    const rv_id = req.body.rv_id;
+    try {
+      const report = await Report.findAll({
+        where: {
+          repo_rv_id: rv_id
+        }
+      });
+      if (report) {
+        return res.json(report);
+      }
+    } catch(error) {
+      console.log('[error] sub DB - report : ', error);
+      return res.redirect('/');
+    }
+  },
+
+  findOne: async (req, res) => {
+    const body = req.body;
+    try {
+      const report = await Report.findOne({
+        where: {
+          repo_rv_id: body.rv_id,
+          reporter: body.reporter
+        }
+      });
+      if (report) {
+        return res.json(report);
+      }
+    } catch(error) {
+      console.log('[error] sub DB - report : ', error);
+      return res.redirect('/');
+    }
+  },
+
+
   delete: async (req, res) => {
     const r_id = req.body.r_id;
     try {
       await Report.destroy({ where: {r_id: r_id} });
       return res.json({});
     } catch (error) {
-      return error;
+      console.log('[error] sub DB - report : ', error);
+      return res.redirect('/');
     }
   }
 }
--- routers/reportRouter.js
+++ routers/reportRouter.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router();
 const reportModel = require('../models/reportModel.js');
 
+// 리뷰 아이디로 신고 테이블에서 검색
+router.get('findAllByID:/rv_id', reportModel.findAllById);
+// 리뷰 아이디, 신고자 이름으로 검색
+router.get('findOne/:rv_id/:reporter', reportModel.findOne);
 // 신고 삭제
 router.post('/delete', reportModel.delete);
 
