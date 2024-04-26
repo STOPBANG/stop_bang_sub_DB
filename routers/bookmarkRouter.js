8a7a76e91d41702c213c42ec785fd08558179bdc
joiejuni <lumiere.298@gmail.com>
2024-04-26 17:37:16 +0900

    feat: 북마크 라우터 추가

--- routers/bookmarkRouter.js
+++ routers/bookmarkRouter.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router();
 const bookmarkModel = require('../models/bookmarkModel.js');
 
+// resident_r_id로 북마크 검색
+router.get('/findAllById/:r_id', bookmarkModel.findALLById);
+// resident_r_id, ra_regno로 북마크 검색
+router.get('/findAllByIdnRegno/:r_id/:ra_regno',
+bookmarkModel.findALLByIdnRegno);
 // 북마크 업데이트
 router.put('/update', bookmarkModel.update);
 // 북마크 삭제
