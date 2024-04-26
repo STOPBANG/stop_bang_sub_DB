204dac9d7b4b2364937ae5bc965ed88a96b7f289
joiejuni <lumiere.298@gmail.com>
2024-04-26 16:17:50 +0900

    docs: sequelize 인스턴스 생성 주석 추가, 들여쓰기

--- database/models/index.js
+++ database/models/index.js
@@ -8,7 +8,12 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config); 
+// Sequelize 인스턴스 생성
+const sequelize = new Sequelize(
+    config.database, 
+    config.username, 
+    config.password, 
+    config); 
 
 db.sequelize = sequelize;
 db.OpenedReview = OpenedReview;
