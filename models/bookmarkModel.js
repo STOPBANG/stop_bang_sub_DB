a5eb5d23be0f8e625342498b3616821107125542
joiejuni <lumiere.298@gmail.com>
2024-04-26 16:50:49 +0900

    feat: bookmark 업데이트 기능

--- models/bookmarkModel.js
+++ models/bookmarkModel.js
@@ -1,14 +1,43 @@
 const Bookmark = require('../database/models/tables/bookmark');
 
 module.exports = {
-  update: async (req, res) => {
-    const body = req.body;
-
+  // 북마크 아이디로 찾기
+  findById: async (req, res) => {
 
   },
-
-  delete: async (req, res) => {
+  
+  // 북마크 업데이트
+  update: async (req, res) => {
     const body = req.body;
+    try {
+      const bookmark = await Bookmark.update(
+        {
+          bm_id: body.bm_id,
+          agentList_ra_regno: body.agentList_ra_regno,
+          resident_r_id: body.resident_r_id
+        },
+        {
+          where: {
+            resident_r_id: body.resident_r_id
+          }
+        }
+      );
+      return res.json(bookmark);
+    } catch (error) {
+        console.log('[error] sub DB : ', error);
+        return res.redirect('/');
+    }
+  },
+
+  // 북마크 삭제
+  delete: async (req, res) => {
+    const bm_id = req.body.r_id;
+    try {
+      await Report.destroy({ where: {bm_id: bm_id} });
+      return res.json({});
+    } catch (error) {
+      return error;
+    }
 
   }
 }
