37c7c584ab9913137dcbce9a54fe26b01673f220
joiejuni <lumiere.298@gmail.com>
2024-04-26 15:24:45 +0900

    docs: 주석 추가

--- routers/reportRouter.js
+++ routers/reportRouter.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const reportModel = require('../models/reportModel.js');
 
+// 신고 삭제
 router.post('/deleteReport', reportModel.deleteReport);
 
 module.exports = router
