e9eb2889f3ea5865eeb350af393ecbdf1ec0bccd
joiejuni <lumiere.298@gmail.com>
2024-04-26 15:37:13 +0900

    feat: 신고 삭제 model 작성

--- models/reportModel.js
+++ models/reportModel.js
@@ -2,7 +2,12 @@ const Report = require('../database/models/tables/report');
 
 module.exports = {
   deleteReport: async (req, res) => {
-    const body = req.body;
-
+    const r_id = req.body.r_id;
+    try {
+      await Report.destroy({ where: {r_id: report_id} });
+      return res.json({});
+    } catch (error) {
+      return error;
+    }
   }
 }
