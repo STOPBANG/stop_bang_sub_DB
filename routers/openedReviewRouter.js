5b3ee703f1089ff865354b5bca8c78a8524cd9bc
yewonahn <yewonahn@naver.com>
2024-05-27 20:19:43 +0900

    fix : 에러 수정

--- routers/openedReviewRouter.js
+++ routers/openedReviewRouter.js
@@ -5,11 +5,6 @@ const openedReviewModel = require('../models/openedReviewModel.js');
 router.get('/findAllById/:r_id', openedReviewModel.findAllById);
 router.post('/findById', openedReviewModel.findById);
 router.post('/create', openedReviewModel.create);
-<<<<<<< Updated upstream
 router.post('/delete', openedReviewModel.delete);
-=======
-// 리뷰 아이디로 삭제
-router.post('/delte', openedReviewModel.delete);
->>>>>>> Stashed changes
 
 module.exports = router
