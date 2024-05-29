3c71dd0a43c226a3f743a262af0742d5b86b0221
hyeon-b <20201009@sungshin.ac.kr>
2024-05-29 14:34:44 +0900

    fix: 열람한 리뷰 추가 시 큐 없음 에러 수정

--- rabbit_opened_review.js
+++ rabbit_opened_review.js
@@ -6,20 +6,24 @@ module.exports = {
         amqp.connect(process.env.RABBIT).then(connection => {
         connection.createChannel().then(messageChannel => {
             const queue = 'opened_review';
+            
+            messageChannel.assertQueue(queue, {}).then(()=>{
+                messageChannel.consume(queue, consumeMessage);            
+            })
 
             function consumeMessage(msg) {
                 console.log("Received a 'opened_reveiw' message");
-                const parsedMsg = JSON.parse(msg.content.toString());
+
+                const parsedMsg = JSON.parse(msg.content);
                 
                 openedReviewModel.create(parsedMsg).then(()=>{
                     console.log("Acknowledging message was handled.");
                     messageChannel.ack(msg);
                 })
             }
-            messageChannel.consume(queue, consumeMessage);
 
         })
-        });
+    });
     },
 
 }
