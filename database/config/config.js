b72076154a53a882fbf15a08f4380f7fa79f0cef
ywonchae1 <ywonchae62@gmail.com>
2024-06-04 12:20:21 +0900

    fix: db config file

--- database/config/config.js
+++ database/config/config.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": "cloudcomputing",
-    "password": "cloudcomputingcc123!",
-    "database": "mydb",
-    "host": "sub_db",
-    "dialect": "mysql"
+  development: {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
   }
 }
